da2e751c32cbf833b85ed6210b4b14fe3fa0ba25
Oğuzhan Acar <ogzhn467@gmail.com>
2018-05-22 12:57:36 +0300

    paketler kuruldu mlab robomongo bağlantısı sağlandı

---
+++ helper/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb://movie_user:1234@ds231460.mlab.com:31460/movie-api');
+    mongoose.connection.on('open', () => {
+        console.log('MongoDb Connected...')
+    });
+    mongoose.connection.on('error', (err) => {
+        console.log('MongoDb ERROR!!!',err)
+    });
+};

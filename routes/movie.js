df43a36a2abd462cffb1148a1b89271aca91afd1
Oğuzhan Acar <ogzhn467@gmail.com>
2018-05-23 10:47:38 +0300

    directora geçiyoruz..

--- routes/movie.js
+++ routes/movie.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Movie = require('../models/Movie');
 
+//bütün filmleri çeken
 router.get('/', (req,res) => {
     const promise = Movie.find({ });
     promise.then((data) => {
@@ -12,23 +13,91 @@ router.get('/', (req,res) => {
     });
 });
 
+// TOP 10 List
+router.get('/top10', (req,res) => {
+  const promise = Movie.find({ }).limit(10).sort({imdb_score: -1});
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+
+// id bazlı film çeken
+router.get('/:movie_id' , (req,res,next) => {
+    const promise = Movie.findById(req.params.movie_id);
+    promise.then((movie) => {
+      if(!movie)
+        next({ message: 'The movie was not found.' , code: 1});
+        res.json(movie);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+//id bazlı film güncelleme
+router.put('/:movie_id' , (req,res,next) => {
+  const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, {new:true} );
+  promise.then((movie) => {
+    if(!movie)
+      next({ message: 'The movie was not found.' , code: 99});
+      res.json(movie);
+  }).catch((err) => {
+      res.json(err);
+  });
+});
+
+//id bazlı film silme
+router.delete('/:movie_id' , (req,res,next) => {
+  const promise = Movie.findByIdAndRemove(req.params.movie_id);
+  promise.then((movie) => {
+    if(!movie)
+      next({ message: 'The movie was not found.' , code: 1});
+      res.json({status: 1});
+  }).catch((err) => {
+      res.json(err);
+  });
+});
+
+// yeni film ekleme
 router.post('/', (req, res, next) => {
  // const { title, imdb_score, category, country, year} = req.body;
     const movie = new Movie (req.body);
 
     const promise = movie.save();
     promise.then((data) => {
-       res.json({ status: 1 });
+       res.json(data);
     }).catch((err) => {
       res.json(err);
     });
 });
 
+// between   gte= büyük ve eşit   lte = küçük ve eşit   lt= KÜÇÜK   GT=BÜYÜK
+router.get('/between/:start_year/:end_year', (req,res) => {
+  const { start_year, end_year } = req.params;
+  const promise = Movie.find(
+    {
+      year: { "$gte": parseInt(start_year), "$lte": parseInt(end_year)  }
+    }
+  );
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  })
+});
+
+
+
+
+
 
 
 module.exports = router;
 
-
+ 
+// route çakışması sıralama ile halledilebiliyor
 
 //const un içinde açtığın objeye destructing deniliyor es6 özelliği gelen datayı bölüp eşleştirdi
 
